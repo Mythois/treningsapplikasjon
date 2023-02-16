f9fc91f17fc8aa9ba9efd5b6466ee577353527e5
TommyChan6 <tommychan661995@gmail.com>
2023-02-17 00:25:55 +0100

    #25 Add Scroll tab on top of homepage for different types of feed

--- screens/mainNavScreens/HomeScreen.tsx
+++ screens/mainNavScreens/HomeScreen.tsx
@@ -1,12 +1,36 @@
 import { Button } from '@rneui/themed';
 import { signOut } from 'firebase/auth';
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, SafeAreaView, ScrollView } from 'react-native'
 import { auth } from '../../firebase';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
+      {/* This is the top section */}
+      <SafeAreaView>
+        <ScrollView horizontal={true}>
+          <View style={styles.feedTab}>
+            <Text style={styles.feedTabText}>All</Text>
+          </View>
+          <View style={styles.feedTab}>
+            <Text style={styles.feedTabText}>Discover</Text>
+          </View>
+          <View style={styles.feedTab}>
+            <Text style={styles.feedTabText}>Friend</Text>
+          </View>
+          <View style={styles.feedTab}>
+            <Text style={styles.feedTabText}>Group</Text>
+          </View>
+          <View style={styles.feedTab}>
+            <Text style={styles.feedTabText}>Myself</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      {/* This is the feed section */}
+      <View>
+
+      </View>
       <Text
                 onPress={() => navigation.navigate('Home')}
                 style={{ fontSize: 26, fontWeight: 'bold'}}
@@ -39,8 +63,8 @@ function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     width: '100%',
     flex: 1,
   },
@@ -48,6 +72,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
+  },
+  feedTab: {
+    padding: 10,
+    borderBottomWidth: 2,
+    marginTop: 30,
+  },
+  feedTabText: {
+    fontSize: 28,
   }
 })
 export default HomeScreen;

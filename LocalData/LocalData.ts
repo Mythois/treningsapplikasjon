7e698537f2dd2f06f7ac26729b8b8f247bac6c95
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-02-17 18:52:53 +0100

    26- feat: create local data class

---
+++ LocalData/LocalData.ts
@@ -0,0 +1,25 @@
+import { WorkoutUser } from "./Users/WorkoutUser";
+import { auth, db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import UsersCollectionLoader from "./Users/UserCollectionLoader";
+
+export class LocalData {
+  static currentUser: WorkoutUser = new WorkoutUser({});
+  
+  static usersCollection: UsersCollectionLoader = new UsersCollectionLoader();
+    
+  static async initCurrentUser() {
+    console.log("Init curr user");
+    
+    const cu = auth.currentUser;
+    if (cu) {
+      const userRef = doc(db, "users", cu.uid);
+      await getDoc(userRef)
+      .then((snap) => {
+        console.log(snap.data());
+        this.currentUser = new WorkoutUser(snap.data());
+      })
+      .catch(error => console.log(error.message));
+    }
+  }
+}

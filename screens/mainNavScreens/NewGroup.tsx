30e984430a010ce60eac9362af81efab2862b5c9
Åshild Føllesdal <amfolle@stud.ntnu.no>
2023-03-17 08:39:22 +0100

    #29 fixing layout

--- screens/mainNavScreens/NewGroup.tsx
+++ screens/mainNavScreens/NewGroup.tsx
@@ -28,6 +28,9 @@ export default function NewGroup({navigation}) {
     const handleSave = () => {
         
     }
+    const handleBack = () => {
+        navigation.navigate('Groups')
+    }
 
     const childRef: any = React.useRef();
 
@@ -35,22 +38,19 @@ export default function NewGroup({navigation}) {
         <View style={styles.container}>
             <SafeAreaView>
             <SmallHeaderContent user={LocalData.currentUser} ref={childRef} navigation={navigation}></SmallHeaderContent>
+            
             </SafeAreaView>
+            
         <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#121212'}}>
             
-            <View style={{marginLeft:"-65%", marginTop:"5%"}}>
-                <Button
-                    onPress={() => navigation.navigate('Groups')}
-                    buttonStyle={{ width:70
-                        , backgroundColor:"#000000", borderColor:"#FFFFFF", borderWidth:1}}
-                    titleStyle={{fontSize:15}}
-
-                    title="Back"
-                />
-            </View>
+            <View style={{marginLeft:"-80%", marginTop:"1%", flexDirection: "row"}}>
+                
+                <Icon type='font-awesome' name="chevron-left" size={20} color={'#e6e6e6'} onPress={handleBack}/>
+                <Text style={styles.text} onPress={handleBack}>Back</Text>
+          </View>
             <View style={{flexDirection:'column',marginTop:"2%"}}>
                     
-                    <View style={{padding:5, marginTop:"5%", width:250, height:40}}>
+                    <View style={{padding:5, marginTop:"5%", width: '85%', height:40}}>
                         <TextInput
                             style={styles.mainText}
                             placeholderTextColor={"#DC6247"}
@@ -58,17 +58,17 @@ export default function NewGroup({navigation}) {
                             onChangeText={handleNameChange}
                         />
                     </View>
-                    <View style={{borderWidth:1, borderColor:"#FFFFFF", borderRadius:5, padding:5, marginTop:"7%", width:250, height:150}}>
+                    <View style={{borderWidth:1, borderColor:"#303030", borderRadius:5, padding:5, marginTop:"7%", width:350, height:300 }}>
                         <TextInput
-                        style={{fontSize:23, color: "#FFFFFF"}}
-                        placeholderTextColor={"#FFFFFF"}
+                        style={styles.boxText}
+                        placeholderTextColor={"#303030"}
                         placeholder="Description"
                         onChangeText={handleDescChange}
                         />
                     </View>
                     <View>
                         <Button
-                        buttonStyle={{borderRadius:5, marginTop:"10%"}}
+                        buttonStyle={{borderRadius:5, marginTop:"10%",backgroundColor: "#136A4A" }}
                         title="Create"
                         onPress={() =>handleSave()}
                         />
@@ -90,6 +90,17 @@ const styles = StyleSheet.create({
         fontWeight: '900',
         letterSpacing: 2,
         color: '#DC6247',
-        textAlign:"center",
+        //textAlign:"center",
+    },
+    text: {
+        color: "#FFFFFF",
+        marginLeft: '1%',
+        fontSize: 15,
+    }, 
+    boxText: {
+        fontSize: 22,
+        fontWeight: '400',
+        letterSpacing: 1,
+        color: '#FFFFFF',
     },
 });
